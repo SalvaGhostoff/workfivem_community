5e31631743fae80799c52346b4a8744b198dfccf
Epyi <68242948+epyidev@users.noreply.github.com>
2024-09-07 10:32:22 +0200

    Update fxmanifest.lua

--- resources/[1_libraries]/RageUI/fxmanifest.lua
+++ resources/[1_libraries]/RageUI/fxmanifest.lua
@@ -20,3 +20,42 @@ files({
 	"src/menu/panels/*.lua",
 	"src/menu/windows/*.lua",
 })
+
+--[[
+
+ HOW TO USE IN OTHER SCRIPTS
+
+ ADD IN FXMANIFEST.LUA :
+ 
+ 	client_scripts({
+		"@RageUI/src/RMenu.lua",
+		"@RageUI/src/menu/RageUI.lua",
+		"@RageUI/src/menu/Menu.lua",
+		"@RageUI/src/menu/MenuController.lua",
+		"@RageUI/src/components/Audio.lua",
+		"@RageUI/src/components/Keys.lua",
+		"@RageUI/src/components/Rectangle.lua",
+		"@RageUI/src/components/Screen.lua",
+		"@RageUI/src/components/Sprite.lua",
+		"@RageUI/src/components/Text.lua",
+		"@RageUI/src/components/Visual.lua",
+		"@RageUI/src/menu/elements/ItemsBadge.lua",
+		"@RageUI/src/menu/elements/ItemsColour.lua",
+		"@RageUI/src/menu/elements/PanelColour.lua",
+		"@RageUI/src/menu/items/UIButton.lua",
+		"@RageUI/src/menu/items/UICheckBox.lua",
+		"@RageUI/src/menu/items/UILine.lua",
+		"@RageUI/src/menu/items/UISlider.lua",
+		"@RageUI/src/menu/items/UISliderHeritage.lua",
+		"@RageUI/src/menu/items/UISliderProgress.lua",
+		"@RageUI/src/menu/panels/UIBoutonPanel.lua",
+		"@RageUI/src/menu/panels/UIColourPanel.lua",
+		"@RageUI/src/menu/panels/UIGridPanel.lua",
+		"@RageUI/src/menu/panels/UIGridPanelHorizontal.lua",
+		"@RageUI/src/menu/panels/UIGridPanelVertical.lua",
+		"@RageUI/src/menu/panels/UIPercentagePanel.lua",
+		"@RageUI/src/menu/panels/UIStatisticsPanel.lua",
+		"@RageUI/src/menu/windows/UIHeritage.lua",
+	})
+
+]]
