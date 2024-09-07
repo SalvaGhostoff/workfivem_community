bd7be0e1eb06ae994cbe9956b9992cd82bdfe393
Epyi <68242948+epyidev@users.noreply.github.com>
2024-09-07 09:30:07 +0200

    feat: add RageUI Library

---
+++ resources/[1_libraries]/RageUI/fxmanifest.lua
@@ -0,0 +1,22 @@
+---
+--- @author Dylan MALANDAIN, Kalyptus
+--- @version 1.0.0
+--- created at [24/05/2021 10:02]
+---
+
+fx_version("cerulean")
+
+games({ "gta5" })
+
+client_scripts({
+	"src/RMenu.lua",
+	"src/menu/RageUI.lua",
+	"src/menu/Menu.lua",
+	"src/menu/MenuController.lua",
+	"src/components/*.lua",
+	"src/menu/elements/*.lua",
+	"src/menu/items/*.lua",
+	"src/menu/panels/*.lua",
+	"src/menu/panels/*.lua",
+	"src/menu/windows/*.lua",
+})
